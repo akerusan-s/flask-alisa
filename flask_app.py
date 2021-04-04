14192c281f6c9c8de48c0f0523dd0919f5646514
ivash <ivashchenko_serezha@mail.ru>
2021-04-04 15:12:15 +0300

    com3

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import logging
 import json
 
@@ -9,6 +9,11 @@ possibleAnswers = [
         'ладно', 'куплю',
         'покупаю', 'хорошо'
     ]
+
+
+@app.route('/')
+def start_screen():
+    return render_template("""<h1>Working Good</ph1>""")
 
 
 @app.route('/post', methods=['POST'])
